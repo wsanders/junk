97b18823b35c5895295d8f57e12f6f18f167e9f9
W Sanders <wsanders@wsanders.net>
2022-10-28 18:37:55 -0700

    Took all out the debug printing
    Use the IDK graphing tool to see results.

--- geiger.ino
+++ geiger.ino
@@ -6,9 +6,11 @@
 // 1 REM = 10000 uSv
 
 #include <util/atomic.h>
+#define ALARM_CPM 70
 const int geiger_pin = 2;
 const int meter_pin = 3;
-const int led_pin = 13;
+const int count_led_pin = 13;
+const int alarm_led_pin = 9;
 const int n=20;
 volatile int pos=0;
 volatile long buf[n];
@@ -16,13 +18,13 @@ volatile int led=0;
 volatile bool triggered=false;
 const int maxcpm=100;
 unsigned long printtimer=0;
-int printlast=0;
-
 
 void setup() {
   pinMode(meter_pin, OUTPUT);
-  pinMode(led_pin, OUTPUT);
-
+  pinMode(count_led_pin, OUTPUT);
+  pinMode(alarm_led_pin, OUTPUT);
+  
+  digitalWrite(alarm_led_pin, HIGH);
   // Ring buffer
   for (int i=0; i<n; i++) { buf[i] = -1; }
 
@@ -34,7 +36,7 @@ void setup() {
 void loop() {
     long avg, cpm;
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {   
-        if (buf[pos] != -1i && triggered) {
+        if (buf[pos] != -1 && triggered) {
             // ms between pulses
             avg = (buf[pos>0 ? pos-1 : n-1] - buf[pos]) / (n -1);
             cpm = min(maxcpm, 60000 / avg);
@@ -42,11 +44,16 @@ void loop() {
             triggered = false;
         }
     }
+    if (cpm > ALARM_CPM) { digitalWrite(alarm_led_pin, HIGH); } 
+    else { digitalWrite(alarm_led_pin, LOW); }
     if ((millis()- printtimer)  > 10000) {
-            printbuffer();
-            Serial.print(avg);
-            Serial.print("\t");
-            Serial.println(cpm);
+//            printbuffer();
+            if (buf[pos] != -1) { 
+//                Serial.print(avg);
+//                Serial.print("\t");
+                Serial.print(cpm);
+            }
+            Serial.println();
             printtimer = millis();
     }
 }
@@ -56,7 +63,7 @@ void isrcount() {
   pos++;
   if (pos > n-1) { pos = 0; }
   led = !led;
-  digitalWrite(led_pin, led);
+  digitalWrite(count_led_pin, led);
   triggered = true;
 }
 
